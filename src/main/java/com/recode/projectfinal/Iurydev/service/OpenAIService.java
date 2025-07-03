209c6dc1c536bea4a7a1470cabb3ca28e6ef03e7
Iury Rodrigues de Godoy <iuryrodriguesdegodoy@gmail.com>
2025-07-03 03:15:39 -0300

    Novo .jar

--- src/main/java/com/recode/projectfinal/Iurydev/service/OpenAIService.java
+++ src/main/java/com/recode/projectfinal/Iurydev/service/OpenAIService.java
@@ -17,7 +17,7 @@ import java.util.concurrent.ConcurrentHashMap;
 @Service
 public class OpenAIService {
 
-    @Value("${openai.api.key:}")
+    @Value("${openai.api.key}")
     private String apiKey;
 
     private boolean apiEnabled = true;
