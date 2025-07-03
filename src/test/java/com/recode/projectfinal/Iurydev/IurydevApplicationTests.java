2bbd3c520cf1ee0aab67a79f8ab23e06a66c2d2c
Iury Rodrigues de Godoy <iuryrodriguesdegodoy@gmail.com>
2025-07-03 15:36:34 -0300

    Atualização das dependências de migrações(flyway)

--- src/test/java/com/recode/projectfinal/Iurydev/IurydevApplicationTests.java
+++ src/test/java/com/recode/projectfinal/Iurydev/IurydevApplicationTests.java
@@ -2,7 +2,9 @@ package com.recode.projectfinal.Iurydev;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
+@ActiveProfiles("test")
 @SpringBootTest
 class IurydevApplicationTests {
 
